0f61f865638d8f03bdc32e6feb4b92ff8640302e
[rauf] <[raufam@code.edu.az]>
2022-12-22 08:50:46 +0400

    init

---
+++ bos2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace bos2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A-ni daxil et:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("B-ni daxil et:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("C-ni daxil et:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double d = a + b + c;
+
+            Console.WriteLine($"{a}+{b}+{c}={d}");
+        }
+    }
+}
